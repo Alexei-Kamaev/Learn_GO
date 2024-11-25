a8c703ae67f7b2612f2811024ac817b63eac6998
Alexei Kamaev <alexei-kamaev@yandex.ru>
2024-11-25 08:59:25 +0300

    5 задач, вроде правильно

---
+++ 1400_task_GO/1-200/3.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	var a, b = 51, 11
+	fmt.Println(a)
+	fmt.Print(b)
+}
---
+++ 1400_task_GO/1-200/4.go
@@ -0,0 +1,10 @@
+package main
+import (
+	"fmt"
+)
+func main () {
+	var a, b, c = 6, 11, 22
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Print(c)
+}
