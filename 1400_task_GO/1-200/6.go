ff6777b718e122473800b427e35c3e2ac2b14427
Alexei Kamaev <alexei-kamaev@yandex.ru>
2024-11-26 21:47:44 +0300

    6-я задача, уменьшение знаков после запятой, без округления

---
+++ 1400_task_GO/1-200/6.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+func main () {
+	pi := 3.1415926
+	fmt.Printf("Дано такое число %f, надо уменьшить разрядность. \n", pi)
+	piInt := int64(pi*1000)
+	piFloat := float64(piInt)/1000
+	fmt.Println("Уменьшенная разрадность, без округлений:", piFloat)
+}
